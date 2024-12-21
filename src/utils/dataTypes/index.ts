8ec7891b67fd907a4e4ddd23c31cff9dbb23aa97
Graham Buck <67022940+ghbuck@users.noreply.github.com>
2024-12-21 09:27:31 -0500

    lot's of directions, so CardinalDirections type, points, and tokens

--- src/utils/dataTypes/index.ts
+++ src/utils/dataTypes/index.ts
@@ -1,2 +1,19 @@
-export type * from './interfaces.js'
-export type * from './types.js'
+import { Point } from './interfaces.js'
+import { CardinalDirection } from './types.js'
+
+export * from './interfaces.js'
+export * from './types.js'
+
+export const cardinalDirections = new Map<CardinalDirection, Point>([
+  ['north', { x: 0, y: -1 }],
+  ['south', { x: 0, y: 1 }],
+  ['east', { x: 1, y: 0 }],
+  ['west', { x: -1, y: 0 }],
+])
+
+export const tokenDirections = new Map<CardinalDirection, string>([
+  ['north', '^'],
+  ['south', 'v'],
+  ['east', '>'],
+  ['west', '<'],
+])
--- src/utils/dataTypes/types.ts
+++ src/utils/dataTypes/types.ts
@@ -1,1 +1,2 @@
 export type PartNum = 1 | 2 | 'all'
+export type CardinalDirection = 'north' | 'south' | 'east' | 'west'
