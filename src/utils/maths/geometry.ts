d446efbec646fff997e498ca983f1567b58db271
Graham Buck <67022940+ghbuck@users.noreply.github.com>
2024-12-26 23:47:44 -0500

    add Manhattan dist calculation to `geometry.ts`

--- src/utils/maths/geometry.ts
+++ src/utils/maths/geometry.ts
@@ -18,3 +18,7 @@ export const findFourthPoint = (p1: Point, p2: Point, p3: Point): Point => {
 export const distanceBetweenPoints = (a: Point, b: Point): number => {
   return Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2)
 }
+
+export const getManhattanDistance = (a: Point, b: Point): number => {
+  return Math.abs(a.x - b.x) + Math.abs(a.y - b.y)
+}
