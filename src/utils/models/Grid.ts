ghbuck/Advent-of-Code

commit a81737f1bdbee75e333ff7345d5e57bce5fee377

feat: some random grid and bounds updates that i didn't end up using, but are worth keeping

src/utils/models/Grid.ts
@@ -30,7 +30,7 @@ export interface RegionResponse<T> {
   regions: RegionInfo[]
 }
 
-export type NeighborLocation = 'above' | 'below' | 'left' | 'right'
+export type NeighborLocation = 'above' | 'below' | 'left' | 'right' | 'aboveLeft' | 'aboveRight' | 'belowLeft' | 'belowRight'
 export type NeighborhoodMap = Map<NeighborLocation, number | undefined>
 
 export type ShiftDirection = NeighborLocation
@@ -382,17 +382,43 @@ export class Grid<T> {
     const colCount = this.#bounds.getMaxX() + 1
 
     const neighborhoodMap: NeighborhoodMap = new Map<NeighborLocation, number>()
-    if (cellKey - colCount > 0) {
-      neighborhoodMap.set('above', cellKey - colCount)
+
+    const aboveKey = cellKey - colCount
+    const belowKey = cellKey + colCount
+    const leftKey = cellKey - 1
+    const rightKey = cellKey + 1
+
+    if (aboveKey > 0) {
+      neighborhoodMap.set('above', aboveKey)
+
+      const aboveLeftKey = aboveKey - 1
+      const aboveRightKey = aboveKey + 1
+
+      if (!Number.isInteger((aboveKey - 1) / colCount)) {
+        neighborhoodMap.set('aboveLeft', aboveLeftKey)
+      }
+      if (!Number.isInteger(aboveKey / colCount)) {
+        neighborhoodMap.set('aboveRight', aboveRightKey)
+      }
     }
-    if (cellKey + colCount <= this.#cells.size) {
-      neighborhoodMap.set('below', cellKey + colCount)
+    if (belowKey <= this.#cells.size) {
+      neighborhoodMap.set('below', belowKey)
+
+      const belowLeftKey = belowKey - 1
+      const belowRightKey = belowKey + 1
+
+      if (!Number.isInteger(belowLeftKey / colCount)) {
+        neighborhoodMap.set('belowLeft', belowLeftKey)
+      }
+      if (!Number.isInteger(belowKey / colCount)) {
+        neighborhoodMap.set('belowRight', belowRightKey)
+      }
     }
-    if (!Number.isInteger((cellKey - 1) / colCount)) {
-      neighborhoodMap.set('left', cellKey - 1)
+    if (!Number.isInteger(leftKey / colCount)) {
+      neighborhoodMap.set('left', leftKey)
     }
     if (!Number.isInteger(cellKey / colCount)) {
-      neighborhoodMap.set('right', cellKey + 1)
+      neighborhoodMap.set('right', rightKey)
     }
 
     const cell = this.#cells.get(cellKey)
@@ -451,6 +477,7 @@ export class Grid<T> {
     const matchingNeighbors = this.#filterTheNeighborhood(cell, theMap, localVisited)
 
     return Promise.all(
+      // eslint-disable-next-line @typescript-eslint/await-thenable
       [...matchingNeighbors.values()].map((neighborKey: number | undefined) => {
         if (neighborKey !== undefined) {
           const neighbor = this.#cells.get(neighborKey)
@@ -515,7 +542,7 @@ export class Grid<T> {
     let perimeter = 0
 
     const sortedCells = [...cells].sort((a, b) => a - b)
-    const neighborDirections: NeighborLocation[] = ['above', 'right', 'below', 'left']
+    const neighborDirections: NeighborLocation[] = ['above', 'right', 'below', 'left', 'aboveLeft', 'aboveRight', 'belowLeft', 'belowRight']
 
     const sideInfo: SideInfo<T>[] = []
 

src/utils/models/Bounds.ts
@@ -110,5 +110,17 @@ export class Bounds {
     return point.y >= 0 && this.#max.y >= point.y
   }
 
+  isEdgeX(point: Point): boolean {
+    return point.x === this.#min.x || point.x === this.#max.x
+  }
+
+  isEdgeY(point: Point): boolean {
+    return point.y === this.#min.y || point.y === this.#max.y
+  }
+
+  isEdge(point: Point): boolean {
+    return this.isEdgeX(point) || this.isEdgeY(point)
+  }
+
   //#endregion
 }

commit 49eed0aa990ede2e53f6e72048f77660cf934a48

fix: add guards for diagonals (a81737f broke 2024 day 12)

src/utils/models/Grid.ts
@@ -378,7 +378,7 @@ export class Grid<T> {
    * @param {number} cellKey
    * @returns {NeighborhoodMap} a map of <NeighborLocation, neighbor cell key>
    */
-  mapTheNeighborhood(cellKey: number): NeighborhoodMap {
+  mapTheNeighborhood(cellKey: number, withDiagonals = false): NeighborhoodMap {
     const colCount = this.#bounds.getMaxX() + 1
 
     const neighborhoodMap: NeighborhoodMap = new Map<NeighborLocation, number>()
@@ -391,27 +391,31 @@ export class Grid<T> {
     if (aboveKey > 0) {
       neighborhoodMap.set('above', aboveKey)
 
-      const aboveLeftKey = aboveKey - 1
-      const aboveRightKey = aboveKey + 1
+      if (withDiagonals) {
+        const aboveLeftKey = aboveKey - 1
+        const aboveRightKey = aboveKey + 1
 
-      if (!Number.isInteger((aboveKey - 1) / colCount)) {
-        neighborhoodMap.set('aboveLeft', aboveLeftKey)
-      }
-      if (!Number.isInteger(aboveKey / colCount)) {
-        neighborhoodMap.set('aboveRight', aboveRightKey)
+        if (!Number.isInteger((aboveKey - 1) / colCount)) {
+          neighborhoodMap.set('aboveLeft', aboveLeftKey)
+        }
+        if (!Number.isInteger(aboveKey / colCount)) {
+          neighborhoodMap.set('aboveRight', aboveRightKey)
+        }
       }
     }
     if (belowKey <= this.#cells.size) {
       neighborhoodMap.set('below', belowKey)
 
-      const belowLeftKey = belowKey - 1
-      const belowRightKey = belowKey + 1
+      if (withDiagonals) {
+        const belowLeftKey = belowKey - 1
+        const belowRightKey = belowKey + 1
 
-      if (!Number.isInteger(belowLeftKey / colCount)) {
-        neighborhoodMap.set('belowLeft', belowLeftKey)
-      }
-      if (!Number.isInteger(belowKey / colCount)) {
-        neighborhoodMap.set('belowRight', belowRightKey)
+        if (!Number.isInteger(belowLeftKey / colCount)) {
+          neighborhoodMap.set('belowLeft', belowLeftKey)
+        }
+        if (!Number.isInteger(belowKey / colCount)) {
+          neighborhoodMap.set('belowRight', belowRightKey)
+        }
       }
     }
     if (!Number.isInteger(leftKey / colCount)) {
@@ -469,11 +473,11 @@ export class Grid<T> {
    * @param visited - a cheeky name for all previously found neighbors
    * @returns
    */
-  async #findMatchingNeighbors([cellKey, cell]: [number, Cell<T>], visited?: Set<number>): Promise<Set<number>> {
+  async #findMatchingNeighbors([cellKey, cell]: [number, Cell<T>], visited?: Set<number>, withDiagonals = false): Promise<Set<number>> {
     const localVisited = visited !== undefined ? visited : new Set<number>()
     localVisited.add(cellKey)
 
-    const theMap = this.mapTheNeighborhood(cellKey)
+    const theMap = this.mapTheNeighborhood(cellKey, withDiagonals)
     const matchingNeighbors = this.#filterTheNeighborhood(cell, theMap, localVisited)
 
     return Promise.all(
@@ -483,7 +487,7 @@ export class Grid<T> {
           const neighbor = this.#cells.get(neighborKey)
           if (neighbor !== undefined) {
             localVisited.add(neighborKey)
-            return this.#findMatchingNeighbors([neighborKey, neighbor], localVisited)
+            return this.#findMatchingNeighbors([neighborKey, neighbor], localVisited, withDiagonals)
           }
         }
       }),
@@ -538,11 +542,14 @@ export class Grid<T> {
    * @param {Set<number>} cells - a set of all item cells in the region
    * @returns {RegionInfo} a RegionInfo object with area, perimeter, and side count
    */
-  #calculateRegionInfo(cells: Set<number>): RegionInfo {
+  #calculateRegionInfo(cells: Set<number>, withDiagonals = false): RegionInfo {
     let perimeter = 0
 
     const sortedCells = [...cells].sort((a, b) => a - b)
-    const neighborDirections: NeighborLocation[] = ['above', 'right', 'below', 'left', 'aboveLeft', 'aboveRight', 'belowLeft', 'belowRight']
+    const neighborDirections: NeighborLocation[] = ['above', 'right', 'below', 'left']
+    if (withDiagonals) {
+      neighborDirections.push('aboveLeft', 'aboveRight', 'belowLeft', 'belowRight')
+    }
 
     const sideInfo: SideInfo<T>[] = []
 
@@ -578,15 +585,15 @@ export class Grid<T> {
    * @param {T} item - an item potentially in the grid
    * @returns {RegionResponse<T>} the item along with a RegionInfo object
    */
-  async findRegions(item: T): Promise<RegionResponse<T>> {
+  async findRegions(item: T, withDiagonals = false): Promise<RegionResponse<T>> {
     let cells = this.#findCells(item)
     const regions: RegionInfo[] = []
 
     while (cells.length > 0) {
       const firstCell = cells[0]
 
-      const regionCells = await this.#findMatchingNeighbors(firstCell)
-      const regionInfo = this.#calculateRegionInfo(regionCells)
+      const regionCells = await this.#findMatchingNeighbors(firstCell, undefined, withDiagonals)
+      const regionInfo = this.#calculateRegionInfo(regionCells, withDiagonals)
 
       regions.push(regionInfo)
 